cf885477413812bad14ebd1cdce7ac02f0afe449
konopoly <kpolyash@gmail.com>
2024-04-09 14:02:43 -0700

    updated dot files

--- nvim/lua/plugins/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "folke/which-key.nvim",
-  "folke/neodev.nvim",
-  "nvim-lua/plenary.nvim",
-}
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,10 +1,36 @@
 return {
-  'nvim-tree/nvim-tree.lua',
-  lazy = true,
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
+  "nvim-tree/nvim-tree.lua",
+  dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-    require("nvim-tree").setup({})
+    local nvimtree = require("nvim-tree")
+    
+    vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+
+    -- recommended settings from nvim-tree documentation
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    nvimtree.setup({
+      view = {
+        width = 35,
+        relativenumber = true,
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+      filters = {
+        custom = { ".DS_Store" },
+      },
+      git = {
+        ignore = false,
+      },
+    })
   end
 }
